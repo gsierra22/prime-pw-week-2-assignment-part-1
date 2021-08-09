0ea2a1a4961e064834902402a61944ac8dba9d88
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2021-08-09 17:57:37 -0500

    completed week 2 part 1 assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -35,7 +35,7 @@ else {console.log ('How about we go home');}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-if( luckyNumber = 2 && adventurous == true ) {console.log( 'Roll the Dice!' );
+if( luckyNumber = 2 && adventurous == 'true' ) {console.log( 'Roll the Dice!' );
 }
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
